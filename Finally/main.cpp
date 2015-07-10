b706b6e69e3e8d7d8f901c5c6f4c3a898fc5f1c6
Brendan Powers <bpowers@908devices.com>
2015-07-09 22:41:32 -0400

    Easier to use, faster Finally implementation

--- Finally/main.cpp
+++ Finally/main.cpp
@@ -4,33 +4,43 @@
 #include <random>
 using namespace std;
 
-//Finally class: Takes a std::function and runs it when the class is destroyed
-//You can use this to always ensure a bit of code is run when a function or scope
-//ends.
-class Finally {
+//ScopedLambda class: Takes a reference to a lambda and runs it when the class is destroyed
+//You can use this to always ensure a bit of code is run when a function or scope ends.
+template <class Lambda>
+class ScopedLambda {
 public:
-    Finally(const function<void()> &func) : m_func(func) {}
-    Finally(const Finally &other) = delete; //No copy constructor, function would execute twice!
+    ScopedLambda(const Lambda &func) : m_func(func) {}
+    ScopedLambda(const ScopedLambda &other) = delete; //No copy constructor, function would execute twice!
 
-    ~Finally()
+    ~ScopedLambda()
     {
         m_func();
     }
 
 private:
-    function<void()> m_func;
+    const Lambda& m_func;
 };
+
+//Some Macro magic here to make the Finally() macro work
+#define FINALLY_CONCAT_PAIR_INNER(x,y) x ## y
+#define FINALLY_CONCAT_PAIR(x,y) FINALLY_CONCAT_PAIR_INNER(x,y)
+
+#define FINALLY_INNER2(lambda, object, ...) auto lambda = [&]() { __VA_ARGS__ ;}; ScopedLambda<decltype(lambda)> object(lambda);
+#define FINALLY_INNER(unique, ...) FINALLY_INNER2(FINALLY_CONCAT_PAIR(finallyLambda, unique), \
+    FINALLY_CONCAT_PAIR(finallyObject, unique), __VA_ARGS__)
+
+//The Finally() macro does the real magic here. It constructs a lambda and ScopedLambda object from the arguments passed to it
+#define Finally(...) FINALLY_INNER(123, __VA_ARGS__)
 
 void testFunc()
 {
     random_device::result_type rtRandomNum; //Random number we will use later
 
-    //Create a finally object on the stack
-    //The code in the lambda will always execute when the function returns.
-    //Variables declared before this function can be accessed and should still be valid.
-    Finally f([&]() {
-        cout << "This will be called when the function exits! " << rtRandomNum << endl;
-    });
+    //Run the code in Finally() at the end of the function. The Finally macro creates
+    //a lambda from the specified block, along with a ScopedLambda object. When the
+    //ScopedLambda object goes out of scope, the lambda will execute.
+    //Variables declared before this macro can be accessed and should still be valid.
+    Finally(cout << "This will be called when the function exits! " << rtRandomNum << endl;)
 
     //Generate random numbuer
     random_device rd;

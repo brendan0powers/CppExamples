b6bbc6d8c82d18bf5b79a3d117a4497ddf3a426e
Brendan Powers <bpowers@908devices.com>
2015-07-12 14:19:29 -0400

    Clean up Finally example to make it more readable

--- Finally/main.cpp
+++ Finally/main.cpp
@@ -30,26 +30,28 @@ private:
     FINALLY_CONCAT_PAIR(finallyObject, unique), __VA_ARGS__)
 
 //The Finally() macro does the real magic here. It constructs a lambda and ScopedLambda object from the arguments passed to it
-#define Finally(...) FINALLY_INNER(123, __VA_ARGS__)
+#define FINALLY(...) FINALLY_INNER(123, __VA_ARGS__)
+
+//50/50 chance of returning true/false
+bool getRandomBool();
 
 void testFunc()
 {
-    random_device::result_type rtRandomNum; //Random number we will use later
+    bool bRandom = false;  //A variable declared before the FINALLY can be used
 
-    //Run the code in Finally() at the end of the function. The Finally macro creates
+    //Run the code in FINALLY() at the end of the function. The FINALLY macro creates
     //a lambda from the specified block, along with a ScopedLambda object. When the
     //ScopedLambda object goes out of scope, the lambda will execute.
     //Variables declared before this macro can be accessed and should still be valid.
-    Finally(cout << "This will be called when the function exits! " << rtRandomNum << endl;)
+    FINALLY(cout << "This will be called when the function exits! " << bRandom << endl;);
 
-    //Generate random numbuer
-    random_device rd;
-    auto midPoint = ((random_device::max() - random_device::min()) / 2) + random_device::min();
-    rtRandomNum = rd();
+    //Assign a random value to the variable
+    //Code in the FINALLY block will have access to the new value
+    bRandom = getRandomBool();
 
     //50% change of each outcome
     //The destructor of Finally runs regardless of how the function exits
-    if(rtRandomNum < midPoint)
+    if(bRandom)
     {
         cout << "Returning Normally" << endl;
         return;
@@ -75,3 +77,15 @@ int main()
     return 0;
 }
 
+
+//not importatnt to the example
+bool getRandomBool()
+{
+    //Generate random numbuer
+    random_device rd;
+
+    //Figure out that the 'mid point' of our range of random numbers is
+    auto midPoint = ((random_device::max() - random_device::min()) / 2) + random_device::min();
+
+    return (rd() < midPoint); //is the random number less than the midpoint?
+}
